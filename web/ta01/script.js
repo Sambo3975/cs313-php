07f772eeb12d74eeca2cf2da7df7c57fde68e0e2
Sambo3975 <samboknight@gmail.com>
2018-09-25 18:16:08 -0600

    Work on ta01

--- web/ta01/script.js
+++ web/ta01/script.js
@@ -1,3 +1,7 @@
 function doButton() {
 	alert("Congrats! You pressed the button! There aren't any prizes or anything.")
 }
+
+function doColorChange() {
+	document.getElementById("div1").style.backgroundColor = document.getElementById("colorBox").value
+}
