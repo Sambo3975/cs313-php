8f71990a67e7a63fa0fee9aebaacdc3ccbbb9721
Sambo3975 <samboknight@gmail.com>
2018-09-29 17:42:33 -0600

    Work on personal page

--- web/home.php
+++ web/home.php
@@ -11,17 +11,19 @@
 
 <body class="w3-2018-red-pear">
 <!-- Navbar -->
-<div id="top">
-	<div class="w3-bar w3-2018-valiant-poppy w3-display-container height100">
-		<h1 class="w3-display-middle">Sam Knight</h1>
+<div class="wrapper">
+	<div id="top">
+		<div class="w3-bar w3-2018-valiant-poppy w3-display-container height100">
+			<h1 class="w3-display-middle">Sam Knight</h1>
+		</div>
+		<div class="w3-bar w3-2018-valiant-poppy">
+			<a href="#" class="w3-bar-item w3-button">Home</a>
+			<a href="#" class="w3-bar-item w3-button">Assignments</a>
+		</div>
 	</div>
-	<div class="w3-bar w3-2018-valiant-poppy">
-		<a href="#" class="w3-bar-item w3-button">Home</a>
-		<a href="#" class="w3-bar-item w3-button">Assignments</a>
+	<div id="main" class="w3-2018-limelight w3-container">
+		<p>Test Text</p>
 	</div>
-</div>
-<div id="main" class="w3-2018-limelight w3-container">
-	<p>Test Text</p>
 </div>
 
 </body>
