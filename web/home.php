a0de1fa8ce0e09a219260934e8d1a11565d7a45e
Sambo3975 <samboknight@gmail.com>
2018-09-29 15:48:58 -0600

    Work on personal page

--- web/home.php
+++ web/home.php
@@ -5,11 +5,11 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 
-<body class="w3-pale-blue">
+<body class="w3-light-blue">
 <!-- Navbar -->
-<div class="w3-bar w3-blue">
-	<a href="#" class="w3-bar-item w3-button w3-blue">Home</a>
-	<a href="#" class="w3-bar-item w3-button w3-blue">Assignments</a>
+<div class="w3-bar w3-indigo">
+	<a href="#" class="w3-bar-item w3-button">Home</a>
+	<a href="#" class="w3-bar-item w3-button">Assignments</a>
 </div>
 </body>
 
