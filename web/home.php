ed5da3699b06e74c64642c60db1ff413e4ae9cc7
Sambo3975 <samboknight@gmail.com>
2018-09-29 16:22:27 -0600

    Work on personal page

--- web/home.php
+++ web/home.php
@@ -3,15 +3,22 @@
 
 <head>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-highway.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-2018.css">
+<link href="https://fonts.googleapis.com/css?family=Meera+Inimai|Monofett" rel="stylesheet">
+<link rel="stylesheet" href="style.css">
+<title>Sam Knight</title>
 </head>
 
-<body class="w3-light-blue">
+<body class="w3-2018-limelight">
 <!-- Navbar -->
-<div class="w3-bar w3-highway-blue">
+<div class="w3-bar w3-2018-valiant-poppy">
+	<h1>Sam Knight</h1>
+</div>
+<div class="w3-bar w3-2018-valiant-poppy">
 	<a href="#" class="w3-bar-item w3-button">Home</a>
 	<a href="#" class="w3-bar-item w3-button">Assignments</a>
 </div>
+<div style="height: 2000px;"></div>
 </body>
 
 </html>
