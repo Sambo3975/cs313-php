3b48b845fa0ab14d9e35ae3ecc7f42f891e03f01
Sambo3975 <samboknight@gmail.com>
2018-09-29 15:52:04 -0600

    Work on personal page

--- web/home.php
+++ web/home.php
@@ -3,11 +3,12 @@
 
 <head>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-highway.css">
 </head>
 
 <body class="w3-light-blue">
 <!-- Navbar -->
-<div class="w3-bar w3-indigo">
+<div class="w3-bar w3-highway-blue">
 	<a href="#" class="w3-bar-item w3-button">Home</a>
 	<a href="#" class="w3-bar-item w3-button">Assignments</a>
 </div>
