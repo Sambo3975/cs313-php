017321d4dac7911e1ed0590a9c4a1b8305e80bbd
Sambo3975 <samboknight@gmail.com>
2018-09-29 15:44:59 -0600

    Work on personal page

---
+++ web/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+<body class="w3-pale-blue">
+<!-- Navbar -->
+<div class="w3-bar w3-blue">
+	<a href="#" class="w3-bar-item w3-button w3-blue">Home</a>
+	<a href="#" class="w3-bar-item w3-button w3-blue">Assignments</a>
+</div>
+</body>
+
+</html>
