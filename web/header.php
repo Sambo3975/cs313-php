8e0428893a6c73a5b0bbe8067e7acd2ddd489ec6
Sambo3975 <samboknight@gmail.com>
2018-09-29 20:31:08 -0600

    Continue on week 2

--- web/header.php
+++ web/header.php
@@ -1,6 +1,7 @@
 <!-- Sidebar -->
-<div class="w3-sidebar w3-bar-block w3-card-4 w3-2018-red-pear" style="display:none" id="mySidebar">
+<div class="w3-sidebar w3-bar-block w3-card-4 w3-2018-red-pear w3-animate-opacity w3-animate-left" style="display:none" id="mySidebar">
   <button onclick="w3_close()" class="w3-bar-item w3-button w3-large w3-2018-valiant-poppy">Close &times;</button>
+	<p class="w3-bar-item w3-large">Assignments</p>
   <p class="w3-bar-item">No Assignments Yet</p>
 	<a href="assignments.php" class="w3-button w3-bar-item">Go to Page</a>
 </div>
