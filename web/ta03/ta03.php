a6d479c8d2404bcd9e09ad16f6e48eaa6a4de8bc
Sambo <samboknight@gmail.com>
2018-10-03 22:27:50 -0600

    Actually put the right things in the right files

--- web/ta03/ta03.php
+++ web/ta03/ta03.php
@@ -1,43 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>TA03</title>
-</head>
-<body>
-	<form id="form" method="post" action="ta03.php">
-		<div id="name">
-			Name:<input type="text" name="name">
-		</div>
-		<div id="email">
-			Email:<input type="email" name="email">
-		</div>
-		<div id="major">
-			Major:
-			<input type="radio" name="major" value="Computer Science">Computer Science
-			<input type="radio" name="major" value="Web Design and Development">Web Design and Development
-			<input type="radio" name="major" value="Computer Information Technology">Compute Information Technology
-			<input type="radio" name="major" value="Computer Engineering">
-		</div>
-		<div id="comments">
-			<textarea name="comment" rows="5" cols="40"></textarea>
-		</div>
-		<div>
-			Continents Visited:
-			<br>
-			<input type="checkbox" name="continent[]" value="North America">North America
-			<input type="checkbox" name="continent[]" value="South America">South America
-			<input type="checkbox" name="continent[]" value="Europe">Europe
-			<input type="checkbox" name="continent[]" value="Asia">Asia
-			<input type="checkbox" name="continent[]" value="Australia">Australia
-			<input type="checkbox" name="continent[]" value="Africa">Africa
-			<input type="checkbox" name="continent[]" value="Antarctica">Antarctica
-		</div>
-		<div>
-			<button name="submit" type="submit" form="form">Submit</button>
-		</div>
-	</form>
-	<div>
-
-	</div>
-</body>
-</html>
+<?php
+//ta02.php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$major = $_POST['major'];
+$comment = $_POST['comment'];
+$continent = $_POST['continent'];
+echo "<h1>Your Input:</h1>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $major;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo "Continents Visited: <br>";
+foreach ($continent as $continentName){
+	echo $continentName;
+	echo "<br>";
+}
+?>
